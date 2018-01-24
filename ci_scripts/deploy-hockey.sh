951543470fdd69b5be1ef14d1f8bbae15cfd1b4f
Bronydell <littleponyapps@gmail.com>
2018-01-24 21:58:24 +0300

    CI Test

--- ci_scripts/deploy-hockey.sh
+++ ci_scripts/deploy-hockey.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@app/build/outputs/apk/release/app-release.apk" \
+-F "ipa=@app/build/outputs/apk/ci/app-ci.apk" \
 -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
 https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions/upload
